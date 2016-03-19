0a78a2fa03a0960611c3c01d51b8c325d2b4d393
sundeep yedida <syedida@shoprunner.com>
2016-03-18 18:41:22 -0700

    added database config

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from micawber.providers import bootstrap_basic
 from micawber.contrib.mcflask import add_oembed_filters
 
-
 # create our little application
 app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_SETTINGS'))
 db = SQLAlchemy(app)
+
+from models import Worm
 
 oembed_providers = bootstrap_basic()
 add_oembed_filters(app, oembed_providers)
